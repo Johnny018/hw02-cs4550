a74202169a719be14169fa16245d1a463f4b06a6
Johnny018 <57376155+Johnny018@users.noreply.github.com>
2021-01-28 14:46:45 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,23 @@
+const buttons = document.querySelectorAll(".calc");
+const result = document.getElementById("resultCalc");
+const calc = document.getElementById("displayCalc");
 
+function total(num){
+  let resultx = eval(num)
+  result.innerHTML = resultx?resultx:''
+}
+for (const button of buttons) {
+  button.addEventListener('click', function(event) {
+    const NumAdd = event.target.innerHTML
+    const result = calc.innerHTML+NumAdd
+    calc.innerHTML = result
+    total(result)
+  })
+}
+
+function remove(){
+    const reCalc = calc.innerHTML
+    editedReCalc = reCalc.slice(0, -1)
+    calc.innerHTML = editedReCalc
+    total(editedReCalc)
+}
